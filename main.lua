3d2e420277972d2b4b4f47bafb08f056c300337f
Jared Pettitt <jaredpettitt@gmail.com>
2022-05-01 15:06:33 -0700

    stopping for now, gfx fuck up

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
+import 'CoreLibs/graphics'
+local gfx <const> = playdate.graphics
+
 __ =
 import 'libraries/underscore'
 
 local play = import('source/scenes/play')
-play.update()
 
 function playdate.update()
 	playdate.drawFPS(0,0)
+	play.update()
+	gfx.clear()
 end
--- source/scenes/play.lua
+++ source/scenes/play.lua
@@ -1,23 +1,37 @@
-import 'CoreLibs/graphics'
-local gfx <const> = playdate.graphics
-
 local play = {}
+
+play.world = {
+	size = 1,
+	grav = -9.8,
+	ground = 10,
+}
 
 play.entities = {
 	player = {
-		pos = { x = 100, y = 100 },
+		pos = { x = 200, y = 120 },
 		rad = 8,
 		vel = { x = 0, y = 0, a = 0 }
 	}
-	}
+}
 		
 play.systems = {
+	gravityUpdate = function(e, w)
+		if 240 - e.pos.y > w.ground then 
+			e.vel.y += w.grav
+		end
+	end,
+	velocityUpdate = function(e)
+		e.pos.x += e.vel.x
+		e.pos.y -= e.vel.y
+	end,
 	playerDraw = function (e)
-		gfx.drawCircleAtPoint(e.pos.x, e.pos.y, e.rad)
-	end
+		gfx.fillCircleAtPoint(e.pos.x, e.pos.y, e.rad)
+	end,
 	}
 	
 play.update = function ()
+	play.systems.gravityUpdate(play.entities.player, play.world)
+	play.systems.velocityUpdate(play.entities.player)
 	play.systems.playerDraw(play.entities.player)
 end
 
