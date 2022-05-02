845b2d9dde1dff2c225e1e42e89e535b121b159e
Jared Pettitt <jaredpettitt@gmail.com>
2022-05-02 11:45:00 -0700

    done for now, movement basically working

--- source/scenes/play.lua
+++ source/scenes/play.lua
@@ -3,16 +3,18 @@ local play = {}
 
 play.world = {
 	size = 1,
-	grav = -9.8,
+	grav = -1.5,
 	ground = 220,
 }
 
 play.entities = {
 	player = {
 		grounded = false,
+		jump = 15,
+		maxSpeed = 10,
 		pos = { x = 200, y = 120 },
-		size = 16,
-		speed = 5,
+		size = 10,
+		speed = 3,
 		state = "",
 		states = {
 			
@@ -22,17 +24,24 @@ play.entities = {
 }
 		
 play.systems = {
-	playerMove = function(e, w)
+	playerControl = function(e, w)
 		if e.grounded then
-			if playdate.buttonIsPressed(playdate.kButtonUp) then
+			if playdate.buttonIsPressed(playdate.kButtonUp) then 
+				e.vel.y += e.jump 
 			end
-			if playdate.buttonIsPressed(playdate.kButtonLeft) then
-				e.vel.x -= e.speed
+			if playdate.buttonIsPressed(playdate.kButtonLeft) then 
+				e.vel.x = math.max(e.vel.x - e.speed, -e.maxSpeed)			
 			end
-			if playdate.buttonIsPressed(playdate.kButtonRight) then
-				e.vel.x += e.speed
+			if playdate.buttonIsPressed(playdate.kButtonRight) then 
+				e.vel.x = math.min(e.vel.x + e.speed, e.maxSpeed)
 			end
-		else
+		end
+	end,
+	playerMovement = function(e, w)
+		if e.grounded 
+		and not playdate.buttonIsPressed(playdate.kButtonLeft) 
+		and not playdate.buttonIsPressed(playdate.kButtonRight) then
+			if math.abs(e.vel.x) > 0.1 then e.vel.x /= 1.5 else e.vel.x = 0 end
 		end
 	end,
 	gravityUpdate = function(e, w)
@@ -43,6 +52,11 @@ play.systems = {
 	velocityUpdate = function(e, w)
 		e.pos.x += e.vel.x
 		e.pos.y = math.min(e.pos.y - e.vel.y, w.ground - (e.size / 2))
+	end,
+	worldWrap = function(e)
+		local halfSize = e.size / 2
+		if e.pos.x > 400 + halfSize then e.pos.x = 0 - halfSize end
+		if e.pos.x < 0 - halfSize then e.pos.x = 400 + halfSize end
 	end,
 	playerDraw = function(e)
 		gfx.fillCircleAtPoint(e.pos.x, e.pos.y, e.size / 2)
@@ -59,12 +73,19 @@ play.systems = {
 	end
 }
 	
-play.update = function ()
-	play.systems.groundDraw(play.world)
+play.update = function()
 	play.systems.setGrounded(play.entities.player, play.world)
-	play.systems.gravityUpdate(play.entities.player, play.world)
-	play.systems.playerMove(play.entities.player)
+	play.systems.playerMovement(play.entities.player)
+	play.systems.playerControl(play.entities.player)
 	play.systems.velocityUpdate(play.entities.player, play.world)
+	play.systems.worldWrap(play.entities.player)
+	play.systems.gravityUpdate(play.entities.player, play.world)
+	
+	play.draw()
+end
+
+play.draw = function()
+	play.systems.groundDraw(play.world)
 	play.systems.playerDraw(play.entities.player)
 end
 
